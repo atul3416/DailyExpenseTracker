52b1502c112d82ed934af3b97d65bdec01bfb9b3
atul3416 <rajsvm02@gmail.com>
2026-01-08 16:28:15 +0530

    add navbar with logout functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import Login from "./components/Login";
+import Navbar from "./components/Navbar";
 import Sign_up from "./components/Sign_up";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
      <BrowserRouter>
+      <Navbar/>
       <Routes>
         <Route path="/signup" element={ <Sign_up/> }> </Route>
         <Route path="/login" element={ <Login/> }></Route>
